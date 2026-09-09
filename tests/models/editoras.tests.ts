385d84afe8ae4b8df656c009ae18358033a86494
Gustavo Tavares Ferreira <pet0160450@aluno.firjansenaisesi.com.br>
2026-09-08 21:25:17 -0300

    fix: alocando testes na pasta models

---
+++ tests/models/editoras.tests.ts
@@ -0,0 +1,7 @@
+describe('Editoras', () => {
+  it.todo('GET /editoras devolve 4 editoras');
+  it.todo('GET /editoras/999 devolve 404');
+  it.todo('POST /editoras cria e devolve 201 com id');
+  it.todo('PUT /editoras/1 atualiza e devolve 200');
+  it.todo('DELETE /editoras/:id devolve 204');
+});
---
+++ tests/models/livros.tests.ts
@@ -0,0 +1,8 @@
+describe('Livros', () => {
+  it.todo('GET /livros devolve 5 livros');
+  it.todo('GET /livros/1 devolve título correto');
+  it.todo('GET /livros/999 devolve 404');
+  it.todo('POST /livros cria e devolve 201 com id');
+  it.todo('PUT /livros/1 atualiza e devolve 200');
+  it.todo('DELETE /livros/5 devolve 204');
+});
