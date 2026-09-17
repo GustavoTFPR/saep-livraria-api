10c96f84e8884a696cd525b6962982f5fdda46a8
Gustavo Tavares Ferreira <pet0160450@aluno.firjansenaisesi.com.br>
2026-09-17 20:13:02 -0300

    test: test: plano de testes de livros.

--- tests/routes/livros.test.ts
+++ tests/routes/livros.test.ts
@@ -6,5 +6,10 @@ beforeEach(resetarBanco);
 afterAll(fecharBanco);
 
 describe('Rotas de livro', () => {
-  it.todo('...');
+  it.todo('GET /livros devolve 200 com 5 livros');
+  it.todo('GET /livros/1 devolve 200 com título "O Hobbit"');
+  it.todo('GET /livros/999 devolve 404');
+  it.todo('POST /livros válido (titulo, paginas, autor_id, editora_id) devolve 201 com id no corpo');
+  it.todo('PUT /livros/1 (paginas) devolve 200 com a paginação nova');
+  it.todo('DELETE /livros/5 devolve 204');
 });
